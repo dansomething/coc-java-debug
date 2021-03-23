e838125719555ccd3415a2f9341589a9079159a2
Dan Hansen <dan@powerproleasing.com>
2021-03-23 09:23:02 -0500

    Upgrade eslint config
    https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   rules: {
